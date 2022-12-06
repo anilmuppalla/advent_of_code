6fe4b44ef18443c19386aed551179ea881c31a7f
Anil Muppalla <anil.kumar.848@gmail.com>
2022-12-05 21:28:03 -0500

    day 5 done

---
+++ 2022/5/part2.py
@@ -0,0 +1,19 @@
+test_stacks = [['Z', 'N'], ['M', 'C', 'D'], ['P']]
+main_stacks = [['Z', 'J', 'G'],['Q', 'L', 'R', 'P', 'W', 'F', 'V', 'C'],['F', 'P', 'M', 'C', 'L', 'G', 'R'],['L', 'F', 'B', 'W', 'P', 'H', 'M'],['G', 'C', 'F', 'S', 'V', 'Q'],['W','H', 'J','Z','M','Q','T','L'],['H','F','S','B','V'],['F','J','Z','S'],['M','C','D','P','F','H','B','T']]
+stacks = main_stacks
+for line in open("in.txt", "r"):
+    line = line.strip()
+    inst = line.split(" ")
+    num_items_to_move = int(inst[1])
+    from_stack = int(inst[3]) - 1
+    to_stack = int(inst[5]) - 1
+    temp_list = []
+    for i in range(0, num_items_to_move):
+        item = stacks[from_stack].pop()
+        temp_list.append(item)
+    for i in range(0, num_items_to_move):
+        item = temp_list.pop()
+        stacks[to_stack].append(item)
+    
+for i in stacks:
+    print(i.pop(), end="")
